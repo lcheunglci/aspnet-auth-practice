2d985338f96ad085b4ef40187e487ecd769becb6
Lawrence LCI <lcheunglci@gmail.com>
2022-07-05 23:56:26 -0700

    Configure the api project

--- ImageGallery.API/Program.cs
+++ ImageGallery.API/Program.cs
@@ -1,8 +1,29 @@
+using ImageGallery.API.Entities;
+using ImageGallery.API.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opts => opts.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+// register the DbContext on the container, getting the connection string from
+// appSettings (note: use this during development; in a production environment,
+// it's better to store the connection string in an environment variable)
+builder.Services.AddDbContext<GalleryContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:ImageGalleryDBConnectionString"]);
+});
+
+// register the repository
+builder.Services.AddScoped<IGalleryRepository, GalleryRepository>();
+
+// register AutoMapper-related services
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -15,11 +36,34 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler(appBuilder =>
+    {
+        appBuilder.Run(async context =>
+        {
+            // ensure generic 500 status code on fault.
+            context.Response.StatusCode = StatusCodes.Status500InternalServerError; ;
+            await context.Response.WriteAsync("An unexpected fault happened. Try again later.");
+        });
+    });
+
+    // The default HSTS value is 30 days. You may want to change this for 
+    // production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseStaticFiles();
 
-app.MapControllers();
+// app.UseAuthorization();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
