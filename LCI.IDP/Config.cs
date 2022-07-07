074a5201d1f44fbd02894aef47f305ba4881e016
Lawrence LCI <lcheunglci@gmail.com>
2022-07-06 23:40:38 -0700

    configure IS to login with the auth code flow

--- LCI.IDP/Config.cs
+++ LCI.IDP/Config.cs
@@ -2,6 +2,7 @@
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
 
+using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 
@@ -22,6 +23,26 @@ namespace LCI.IDP
 
         public static IEnumerable<Client> Clients =>
             new Client[]
-            { };
+            {
+                new Client
+                {
+                    ClientName = "Image Gallery",
+                    ClientId= "imagegalleryclient",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string>()
+                    {
+                        "https://localhost:44389/sigin-oidbc"
+                    },
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    },
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    }
+                }
+            };
     }
 }
