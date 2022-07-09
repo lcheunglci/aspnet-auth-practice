75031cc24ef95ebe4f9757bbc243e01a32fcd62c
Lawrence LCI <lcheunglci@gmail.com>
2022-07-08 23:51:25 -0700

    Add logout of the client web app

--- ImageGallery.Client/Controllers/GalleryController.cs
+++ ImageGallery.Client/Controllers/GalleryController.cs
@@ -1,6 +1,7 @@
 ﻿using ImageGallery.Client.ViewModels;
 using ImageGallery.Model;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
@@ -174,6 +175,11 @@ namespace ImageGallery.Client.Controllers
             response.EnsureSuccessStatusCode();
 
             return RedirectToAction("Index");
+        }
+
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
         }
 
         public async Task WriteOutIdentityInformation()
