6feb629343223671c5baf7bb2546c7b791cdff43
Lawrence LCI <lcheunglci@gmail.com>
2022-07-09 00:18:36 -0700

    Add logout to the IDP

--- ImageGallery.Client/Controllers/GalleryController.cs
+++ ImageGallery.Client/Controllers/GalleryController.cs
@@ -2,6 +2,7 @@
 using ImageGallery.Model;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Protocols.OpenIdConnect;
@@ -180,6 +181,7 @@ namespace ImageGallery.Client.Controllers
         public async Task Logout()
         {
             await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
         }
 
         public async Task WriteOutIdentityInformation()
