0c01290299b0ac8fafa13c82387a2d972307046c
Lawrence LCI <lcheunglci@gmail.com>
2022-07-13 23:34:30 -0700

    Update the OrderFrame functionality to be authorized by CanOrderFrame policy

--- ImageGallery.Client/Controllers/GalleryController.cs
+++ ImageGallery.Client/Controllers/GalleryController.cs
@@ -195,7 +195,7 @@ namespace ImageGallery.Client.Controllers
             await HttpContext.SignOutAsync(OpenIdConnectDefaults.AuthenticationScheme);
         }
 
-        [Authorize(Roles = "PayingUser")]
+        [Authorize(Policy = "CanOrderFrame")]
         public async Task<IActionResult> OrderFrame()
         {
             var idpClient = _httpClientFactory.CreateClient("IDPClient");
