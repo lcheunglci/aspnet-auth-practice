722a290fa386bf810481b58fcddf013783a03bf9
Lawrence LCI <lcheunglci@gmail.com>
2022-07-15 00:59:15 -0700

    Add LoadCerticateFromStore functionality and instructions for using a proper cert for signing

--- LCI.IDP/Startup.cs
+++ LCI.IDP/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace LCI.IDP
 {
@@ -38,6 +40,9 @@ namespace LCI.IDP
 
             // not recommended for production - you need to store your key material somewhere secure
             builder.AddDeveloperSigningCredential();
+
+            // Use the following in production
+            //builder.AddSigningCredential(LoadCertificateFromStore());
         }
 
         public void Configure(IApplicationBuilder app)
@@ -59,6 +64,26 @@ namespace LCI.IDP
             {
                 endpoints.MapDefaultControllerRoute();
             });
+        }
+
+        public X509Certificate2 LoadCertificateFromStore()
+        {
+            // i.e. call New-SelfSignedCertificate -Subject "CN=LCIIdSrvSigningCert" -CertStoreLocation "cert:\LocalMachine\My" in admin powershell
+            // and add that cert from Personal/Certificates to Trusted Root Certification/Certificates from Start > Manage Computer Certificate
+            // and grab the thumbprint and paste it below
+            string thumbPrint = "paste_in_thumb_print";
+
+            using (var store = new X509Store(StoreName.My, StoreLocation.LocalMachine))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                var certCollection = store.Certificates.Find(X509FindType.FindByThumbprint,
+                    thumbPrint, true);
+                if (certCollection.Count == 0)
+                {
+                    throw new Exception("The specified certificate was not found");
+                }
+                return certCollection[0];
+            }
         }
     }
 }
