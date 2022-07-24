57c047b92dff940bb34aeff1845a9df5fa069917
Lawrence LCI <lcheunglci@gmail.com>
2022-07-23 23:30:38 -0700

    Implement User Registration

---
+++ LCI.IDP/Quickstart/UserRegistration/RegisterUserViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace LCI.IDP.UserRegistration
+{
+    public class RegisterUserViewModel
+    {
+        [MaxLength(200)]
+        [Display(Name = "Username")]
+        public string UserName { get; set; }
+
+        [MaxLength(200)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        [Display(Name = "Given name")]
+        public string GivenName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        [Display(Name = "Family name")]
+        public string FamilyName { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+        [Required]
+        [MaxLength(2)]
+        [Display(Name = "Country")]
+        public string Country { get; set; }
+
+        public SelectList CountryCodes { get; set; } = new SelectList(
+            new[]
+            {
+                new { Id = "BE", Value = "Belgium"},
+                new { Id = "US", Value = "United States of America"},
+                new { Id = "IN", Value = "India"},
+                new { Id = "CA", Value = "Canada"} },
+                "Id",
+                "Value");
+
+        public string ReturnUrl { get; set; }
+
+
+    }
+}
---
+++ LCI.IDP/Quickstart/UserRegistration/UserRegistrationController.cs
@@ -0,0 +1,99 @@
+﻿using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Services;
+using LCI.IDP.Services;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace LCI.IDP.UserRegistration
+{
+    public class UserRegistrationController : Controller
+    {
+        private readonly ILocalUserService _localUserService;
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public UserRegistrationController(ILocalUserService localUserService, IIdentityServerInteractionService interaction)
+        {
+            _localUserService = localUserService ?? throw new System.ArgumentNullException(nameof(localUserService));
+            _interaction = interaction ?? throw new System.ArgumentNullException(nameof(interaction));
+        }
+
+        [HttpGet]
+        public IActionResult RegisterUser(string returnUrl)
+        {
+            var vm = new RegisterUserViewModel()
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RegisterUser(RegisterUserViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var userToCreate = new Entities.User
+            {
+                UserName = model.UserName,
+                Subject = Guid.NewGuid().ToString(),
+                // Email = model.Email,
+                Active = false
+            };
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type = "country",
+                Value = model.Country
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type = JwtClaimTypes.Address,
+                Value = model.Address
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type = JwtClaimTypes.GivenName,
+                Value = model.GivenName
+            });
+
+            userToCreate.Claims.Add(new Entities.UserClaim()
+            {
+                Type = JwtClaimTypes.FamilyName,
+                Value = model.FamilyName
+            });
+
+            _localUserService.AddUser(userToCreate);
+            await _localUserService.SaveChangesAsync();
+
+            // log the user in
+            await HttpContext.SignInAsync(new IdentityServerUser(userToCreate.Subject)
+            {
+                DisplayName = userToCreate.UserName
+            });
+            // await HttpContext.SignInAsync(userToCreate.Subject, userToCreate.UserName);
+
+
+            // continue with the flow
+            if (_interaction.IsValidReturnUrl(model.ReturnUrl)
+                || Url.IsLocalUrl(model.ReturnUrl))
+            {
+                return Redirect(model.ReturnUrl);
+            }
+
+            return Redirect("~/");
+
+        }
+
+    }
+}
